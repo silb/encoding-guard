3dc8ac9801c179134a2798933c66e1075b8d6e65
Stig Inge Lea Bjørnsen <stiginge@pvv.org>
2013-04-06 14:05:05 +0200

    Switch on 1 and case with euro character literal and it character codes.

--- src/EncodingGuard.java
+++ src/EncodingGuard.java
@@ -7,10 +7,11 @@ public class EncodingGuard {
     static {
         // The Euro sign has different byte representation in UTF-8, ISO-8859-15
         // and CP-1252. The encoding of this file should be UTF-8.
-        switch (0x20AC) { // Euro, UTF-8
+        switch (1) {
         case '€': break; // Euro in the encoding of this file
         case 0xA4: break; // Euro, ISO-8859-15
         case 0x80: break; // Euro, CP-1252
+        //case 0x20AC: break; // Euro, UTF-8.
         }
     }
 }
